1585b645c4b24c11de8e3cf91ddb467be8f9effa
azizu06 <ab725492@ucf.edu>
2026-01-02 12:13:40 -0500

    Make logic for hitting and retrieving data from API

---
+++ src/getData.js
@@ -0,0 +1,19 @@
+import { KEY, BASE } from './config';
+
+export async function getLocation(location) {
+  try {
+    const url = `${BASE}${location}?key=${KEY}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const newData = {
+      location: data.resolvedAddress,
+      temp: data.currentConditions.temp,
+      actual: data.currentConditions.feelslike,
+      wind: data.currentConditions.windspeed,
+      humidity: data.currentConditions.humidity,
+    };
+    return newData;
+  } catch (err) {
+    alert(err);
+  }
+}
