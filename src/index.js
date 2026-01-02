ac2095fa83c52ee7bb4e95eea3cee6feac2f00cb
azizu06 <ab725492@ucf.edu>
2026-01-02 12:13:56 -0500

    Connect all logic in main module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './styles.css';
+import { getLocation } from './getData';
+import { render } from './display';
+
+const search = document.querySelector('.box');
+search.addEventListener('keydown', async (e) => {
+  if (e.key === 'Enter') {
+    const location = search.value.trim();
+    const data = await getLocation(location);
+    render(data);
+  } else {
+    return;
+  }
+});
