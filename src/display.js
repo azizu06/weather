dae533a6f5c5456861c7c6de9e1ef3357d5291d1
azizu06 <ab725492@ucf.edu>
2026-01-02 16:21:39 -0500

    Add temp converting and weather icon features

--- src/display.js
+++ src/display.js
@@ -1,9 +1,10 @@
-export function render(data) {
+export async function render(data) {
   const location = document.querySelector('.location');
   const temp = document.querySelector('.temp');
   const actual = document.querySelector('.actual');
   const wind = document.querySelector('.wind');
   const humid = document.querySelector('.humid');
+  const img = document.querySelector('.wIcon');
   location.innerText = data.location
     .split(' ')
     .map((word) => word[0].toUpperCase() + word.slice(1))
@@ -12,4 +13,29 @@ export function render(data) {
   actual.innerText = 'Feels Like: ' + data.actual + ' °F';
   wind.innerText = 'Wind: ' + data.wind + ' MPH';
   humid.innerText = 'Humidity: ' + data.humidity + '%';
+  try {
+    const icon = await import(`./assets/${data.icon}.svg`);
+    img.src = icon.default;
+  } catch {
+    const safe = await import('./assets/clear-day.svg');
+    img.src = safe.default;
+  }
+}
+
+export function convertTemp(btn, temp1, temp2, temp) {
+  if (btn.innerText === '°C') {
+    const btnF = document.querySelector('.tempF');
+    btn.classList.add('active');
+    btnF.classList.remove('active');
+    const celTemp = (temp - 32) * (5 / 9);
+    temp1.innerText = `${Number(celTemp.toFixed(1))} °C`;
+    temp2.innerText = `Feels Like: ${Number(celTemp.toFixed(1))} °C`;
+  } else {
+    const btnC = document.querySelector('.tempC');
+    btn.classList.add('active');
+    btnC.classList.remove('active');
+    const fahrTemp = temp * (9 / 5) + 32;
+    temp1.innerText = `${Number(fahrTemp.toFixed(1))} °F`;
+    temp2.innerText = `Feels Like: ${Number(fahrTemp.toFixed(1))} °F`;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './styles.css';
 import { getLocation } from './getData';
-import { render } from './display';
+import { render, convertTemp } from './display';
 
+const fahr = document.querySelector('.tempF');
+const cel = document.querySelector('.tempC');
 const search = document.querySelector('.box');
 search.addEventListener('keydown', async (e) => {
   if (e.key === 'Enter') {
@@ -17,5 +19,19 @@ async function init() {
   const data = await getLocation('Orlando Florida');
   render(data);
 }
+
+fahr.addEventListener('click', () => {
+  const temp = document.querySelector('.temp');
+  const temp2 = document.querySelector('.actual');
+  const tempNum = parseFloat(temp.innerText);
+  convertTemp(fahr, temp, temp2, tempNum);
+});
+
+cel.addEventListener('click', () => {
+  const temp = document.querySelector('.temp');
+  const temp2 = document.querySelector('.actual');
+  const tempNum = parseFloat(temp.innerText);
+  convertTemp(cel, temp, temp2, tempNum);
+});
 
 init();
